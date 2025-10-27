65fffeb490dc1ad33c2a983ad0841e0a4da0f8f9
megumiB-0 <megumi.b.design@gmail.com>
2025-10-28 06:17:56 +0900

    add spring_kadai_form

---
+++ spring_kadai_form/src/main/java/com/example/springkadaiform/controller/ContactFormController.java
@@ -0,0 +1,42 @@
+package com.example.springkadaiform.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.springkadaiform.form.ContactForm;
+
+
+
+@Controller
+public class ContactFormController {
+	//フォーム入力画面表示
+	@GetMapping("/form")
+	public String showForm(Model model) {
+		// モデルにからのフォームをセット
+		model.addAttribute("contactForm",new ContactForm());
+		return "contactFormView";
+	}
+	
+	//フォーム送信（POST）
+	@PostMapping("/form")
+	public String submitForm(
+			@Validated ContactForm form, //入力を受け取る
+			BindingResult result,		 //チェック結果を受け取る
+			Model model) {				 //画面にデータを渡す
+		if(result.hasErrors()) {
+			//formにリダイレクトして再表示
+			return "contactFormView";
+		}
+		//入力OK→確認画面へ
+		model.addAttribute("contactForm",form);
+		
+		// confirmにリダイレクトしてリストを再表示
+		return "confirmView";
+	}
+	
+
+}
